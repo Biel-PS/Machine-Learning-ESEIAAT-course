fcb313e5d8f7251acd51674761e051a068348cc2
Biel Pujadas Suriol <66383311+Biel-PS@users.noreply.github.com>
2025-03-30 20:46:48 +0200

    WORKING!!

--- LESON 5/LESON5.m
+++ LESON 5/LESON5.m
@@ -1,6 +1,6 @@
 clear;clc;close all;
 noiseMagData = 0;
-noiseMagX = 10;
+noiseMagX = 100;
 theta = [-0.2,0.8,0.1,0.1,0.1,-0.3,1,0.4,0.1];
 dataVolume = 3;
 
@@ -15,20 +15,20 @@ data = dataClass.generateDataX(sampleVolume,dataVolume,noiseMagX,start);
 data = dataClass.generateDataY(theta,noiseMagData,dataSet);
 
 
-x2L1 = (data.x(:,2)*theta(2)+theta(1))/theta(3);
-x2L2 = (data.x(:,2)*theta(5)+theta(4))/theta(6);
-x2L3 = (data.x(:,2)*theta(8)+theta(7))/theta(9);
+x2L1 = -(data.x(:,2)*theta(2)+theta(1))/theta(3);
+x2L2 = -(data.x(:,2)*theta(5)+theta(4))/theta(6);
+x2L3 = -(data.x(:,2)*theta(8)+theta(7))/theta(9);
 
 figure();
 hold on
 
-scatter(data.x(data.y(:,1), 2), data.x(data.y(:,1), 3), 50, 'g', 'filled', 'DisplayName', 'top');
-scatter(data.x(data.y(:,2), 2), data.x(data.y(:,2), 3), 50, 'r', 'filled', 'DisplayName', 'bottom');
-scatter(data.x(data.y(:,3), 2), data.x(data.y(:,3), 3), 50, 'y', 'filled', 'DisplayName', 'mid');
+scatter(data.x(data.y(:,1), 2), data.x(data.y(:,1), 3), 50, 'g', 'filled', 'DisplayName', 'data1');
+scatter(data.x(data.y(:,2), 2), data.x(data.y(:,2), 3), 50, 'r', 'filled', 'DisplayName', 'data2');
+scatter(data.x(data.y(:,3), 2), data.x(data.y(:,3), 3), 50, 'y', 'filled', 'DisplayName', 'data3');
 
-scatter (data.xLine(:,2),x2L1,'b')
-scatter (data.xLine(:,2),x2L2,'green')
-scatter (data.xLine(:,2),x2L3,'yellow')
+scatter (data.xLine(:,2),x2L1,'g', 'filled', 'DisplayName', 'data1')
+scatter (data.xLine(:,2),x2L2,'r', 'filled', 'DisplayName', 'data2')
+scatter (data.xLine(:,2),x2L3,'y', 'filled', 'DisplayName', 'data3')
 
 hold off
 title("X1 vs X2");
